02531aade1397798716ec8ba2ab30cb5f1444d30
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-05-21 14:38:56 +0000

    上传adodb的扩展。。。。。。。。。。。。。。。。。。。。。

--- trunk/ math-duodaa/duodaaphp/textphp/access_conn.php
+++ trunk/ math-duodaa/duodaaphp/textphp/access_conn.php
@@ -1,1 +1,20 @@
 <?php
+include('adodb5/adodb.inc.php');
+ 
+
+$db = ADONewConnection('ado_access'); 
+$access = realpath('./access/duodaa1.mdb'); 
+$myDSN = 'PROVIDER=Microsoft.Jet.OLEDB.4.0;'.'DATA SOURCE='. $access . ';'."Jet OLEDB:Database Password=123456;"; 
+
+$db->Connect($myDSN); 
+$recordSet = $db->Execute('select * from users'); 
+print '记录条数:';
+print $recordSet->RecordCount();
+print '<br />';
+if (!$recordSet) 
+print $db->ErrorMsg(); 
+else 
+while (!$recordSet->EOF) { 
+print $recordSet->fields[1].' '.$recordSet->fields[2].'<BR>'; 
+$recordSet->MoveNext(); 
+} 
