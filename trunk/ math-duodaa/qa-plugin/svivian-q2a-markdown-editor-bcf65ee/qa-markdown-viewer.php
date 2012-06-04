724254126cb8319c5497448c5f9bef1a361c0da7
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-06-04 10:13:12 +0000

    新版。。。。。。。。。。。。。。。。。。

---
+++ trunk/ math-duodaa/qa-plugin/svivian-q2a-markdown-editor-bcf65ee/qa-markdown-viewer.php
@@ -0,0 +1,45 @@
+<?php
+/*
+	Question2Answer Markdown editor plugin, v2
+	License: http://www.gnu.org/licenses/gpl.html
+*/
+
+class qa_markdown_viewer
+{
+	var $path;
+
+	function load_module($directory, $urltoroot)
+	{
+		$this->path = $directory;
+	}
+
+	function calc_quality($content, $format)
+	{
+		if ( $format == 'markdown' )
+			return 1.0;
+		else
+			return 0.8;
+	}
+
+	function get_html($content, $format, $options)
+	{
+		if ( isset( $options['blockwordspreg'] ) )
+		{
+			require_once QA_INCLUDE_DIR.'qa-util-string.php';
+			$content = qa_block_words_replace( $content, $options['blockwordspreg'] );
+		}
+
+		require_once $this->path . 'inc.markdown.php';
+		$html = Markdown( $content );
+		return qa_sanitize_html($html);
+	}
+
+	function get_text($content, $format, $options)
+	{
+		$viewer = qa_load_module( 'viewer', '' );
+		$text = $viewer->get_text( $content, 'html', array() );
+
+		return $text;
+	}
+
+}
