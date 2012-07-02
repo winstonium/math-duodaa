f7bd94cd90596b47d47ed366cd3ddd7ce2780e72
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-07-02 14:13:38 +0000

    上传。。。。。。。。。。。。。。。。。。。。。。。。。。

---
+++  math-duodaa/iwebsns/iweb_mini_lib/conf/dbconf.php
@@ -0,0 +1,12 @@
+<?php
+$host="localhost:3306";//mysql数据库服务器,比如localhost:3306
+$user="root"; //mysql数据库默认用户名
+$pwd="123456"; //mysql数据库默认密码
+$db="iwebsns"; //默认数据库名
+global $tablePreStr;//设置外部变量
+$tablePreStr="iwebsns_";//表前缀
+
+//当前提供服务的mysql数据库
+global $dbServs;
+$dbServs=array($host,$db,$user,$pwd);
+?>
