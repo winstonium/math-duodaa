cdcd824502f1ecc139e16f133e994df6ed2d7005
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-07-09 13:54:32 +0000

    ...........................

--- trunk/ math-duodaa/iwebsns/login_from_qa.php
+++ trunk/ math-duodaa/iwebsns/login_from_qa.php
@@ -15,9 +15,45 @@
 	require_once $qa_root_to_sns.'/qa-include/qa-base.php';
 	require_once $qa_root_to_sns.'/qa-include/qa-app-users.php';
 
-if(qa_get_logged_in_userid()!='')
-{echo $_GET['e'];}
-else
-{echo '非法请求'; exit;}
+	//如果主站没有登录直接中止运行
+	if(qa_get_logged_in_userid()=='')
+	{
+		echo '非法请求'; 
+		exit;
+	}
+
+	//如果主站登录，分两个情况
+	if(qa_get_logged_in_userid()!='')
+	{  
+		//数据表定义区
+		$t_users=$tablePreStr."users";
+    	$t_group_members=$tablePreStr."group_members";
+		$t_online=$tablePreStr."online";
+		$t_mypals=$tablePreStr."pals_mine";
+		$t_frontgroup=$tablePreStr."frontgroup";
+		
+		//定义读操作
+		$u_email=$_GET['e'];
+		dbtarget('r',$dbServs);
+		$dbo=new dbex;
+		$sql="select * from $t_users where user_email='$u_email'";
+		$user_info=$dbo->getRow($sql);
+		
+		if(empty($user_info))
+		{
+			echo $u_email.'还没有激活。';
+		}
+		
+		if(get_sess_userid()==$user_info['user_id'])
+		{
+			echo $u_email.'没有登录。';			
+		}
+		
+	}
+    else
+    {
+    	echo '非法请求'; 
+    	exit;
+    }
 
 ?>
