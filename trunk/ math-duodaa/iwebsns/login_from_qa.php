880df56a4574a6eb9fd3410baccc58d68e09b7c1
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-07-13 09:59:58 +0000

    修改数值代码.................

--- trunk/ math-duodaa/iwebsns/login_from_qa.php
+++ trunk/ math-duodaa/iwebsns/login_from_qa.php
@@ -15,6 +15,13 @@
 	require_once $qa_root_to_sns.'/qa-include/qa-base.php';
 	require_once $qa_root_to_sns.'/qa-include/qa-app-users.php';
 
+	//如果没有e参数数不对直接中止运行
+	if(!isset($_GET['e']) || qa_get_logged_in_email()!=$_GET['e'])
+	{
+		echo '非法请求'; 
+		exit;
+	}
+	
 	//如果主站没有登录直接中止运行
 	if(qa_get_logged_in_userid()=='')
 	{
@@ -33,7 +40,7 @@
 		$t_frontgroup=$tablePreStr."frontgroup";
 		
 		//定义读操作
-		$u_email=$_GET['e'];
+		$u_email=qa_get_logged_in_email();
 		dbtarget('r',$dbServs);
 		$dbo=new dbex;
 		$sql="select * from $t_users where user_email='$u_email'";
@@ -42,10 +49,60 @@
 		//如果sns里没有这个email帐号，就注册 一个然后登录
 		if(empty($user_info))
 		{
-            echo '<html>'."\n";
-			echo '<head></head>'."\n";
+           //下面新建一个$u_email的账号
+           
+		   //下面定义新建信息的变量
+		   $user_name=qa_get_logged_in_handle();
+		   $user_pws=substr(md5(rand(0,1000000)),0,10);
+		   $user_sex=1;
+		   $user_email=$u_email;
+		   $is_pass=1;
+		   $user_ico=($user_sex==0)?"skin/$skinUrl/images/d_ico_0_small.gif":"skin/$skinUrl/images/d_ico_1_small.gif";
+	       
+		   //$sql="select user_id from $t_users where user_email='$user_email'";
+           //$user_info=$dbo->getRow($sql);
+          // $sort_rs = api_proxy("pals_sort_def");
+		   
+		   //写入数据
+		   dbtarget('w',$dbServs);
+	       $sql="insert into $t_users (user_name,user_pws,user_sex,user_email,user_add_time,user_ico,invite_from_uid,is_pass,lastlogin_datetime,birth_year , birth_month , birth_day ,login_ip )"
+					." values('$user_name','$user_pws',$user_sex,'$user_email','".constant('NOWTIME')."','$user_ico',0,$is_pass,'".constant('NOWTIME')."','','','','$_SERVER[REMOTE_ADDR]')";//$invite_fromuid直接置为0
+
+		   if(!$dbo->exeUpdate($sql))
+		   {
+			  action_return(0,$re_langpackage->re_reg_false,"-1");
+		   }
+
+	       $user_id=mysql_insert_id();
+	       $now_time=time();
+
+	       $sql="insert into $t_online (user_id,user_name,user_sex,user_ico,active_time,hidden) values ($user_id,'$user_name',$user_sex,'$user_ico','$now_time',0)";
+	       $dbo->exeUpdate($sql);
+/*
+			foreach($sort_rs as $rs){
+				$sort_id=$rs['id'];
+				$sort_name=$rs['name'];
+				$sql="insert into $t_pals_sort ( name , user_id ) values ( '$sort_name' , $user_id )";
+				$dbo->exeUpdate($sql);
+			}
+*/			
+			//写入session登录
+			set_sess_userid($user_id);
+		    set_sess_usersex($user_sex);
+		    set_sess_username($user_name);
+		    set_sess_userico($user_ico);
+		    set_sess_online('0');
+			
+			
+		   //结束后提示一些必要信息
+			echo '<html>'."\n";
+			echo '<head>'."\n";
+			echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+			echo '</head>'."\n";
 			echo '<body>'."\n";
-            echo '你的空间账号已经激活。<br /><span id="leftTime">3</span>秒后重新进入空间主页.......'."\n";
+            echo '你的空间账号已经激活。<br />'."\n";
+            echo '注意，你空间的<span style="color:red;">性别信息默认为"女"</span>，请登录后自行修改！<br>'."\n";
+            echo '<span id="leftTime">3</span>秒后重新进入空间主页.......'."\n";
 			echo '<script>var leftsec=3;function minusTime(){if(leftsec!=0){document.getElementById("leftTime").innerHTML=leftsec--;}else{alert("hahaha");leftsec--;}}'."\n";
 			echo 'setInterval("minusTime();",1000)</script>'."\n";
 			echo '</body>'."\n";
