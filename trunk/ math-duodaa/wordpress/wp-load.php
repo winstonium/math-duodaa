7b0c1ffae1f1e5fc92d9aff865e778f69b68ff0d
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-07-30 07:55:24 +0000

    修改wordpress的登录方式.............

--- trunk/ math-duodaa/wordpress/wp-load.php
+++ trunk/ math-duodaa/wordpress/wp-load.php
@@ -59,3 +59,66 @@ if ( file_exists( ABSPATH . 'wp-config.php') ) {
 
 	wp_die( $die, __( 'WordPress &rsaquo; Error' ) );
 }
+
+/**如果没有登录，就跳回首页 **/
+ 	
+//if(!is_user_logged_in()){wp_redirect('http://'.$_SERVER['HTTP_HOST']);exit;}
+
+	
+	//添加整合Question2Answer的引用
+		
+	require_once $_SERVER['DOCUMENT_ROOT'].'/qa-include/qa-base.php';
+	require_once $_SERVER['DOCUMENT_ROOT'].'/qa-include/qa-app-users.php';
+	
+	if(!qa_is_logged_in())
+	{
+	wp_redirect('http://'.$_SERVER['HTTP_HOST'].'?qa=login&to=wordpress');	
+	exit;
+	}
+	
+	elseif(!is_user_logged_in())
+	{   
+		$useremail=qa_get_logged_in_email();
+		$userhandle=qa_get_logged_in_handle();
+		//echo '<script>alert("'.$useremail.'");</script>';
+		
+
+		if(email_exists($useremail))
+	    {
+	    	$qa_user = get_user_by('email', $useremail);
+	    	wp_set_auth_cookie($qa_user->ID, true, false);
+	        do_action('wp_login', $qa_user->user_login, $qa_user);
+	        
+	        echo '<script>window.location.reload(); </script>';
+	        exit;
+	    	//var_dump($qa_user);
+	    	//wp_set_current_user(null,$userhandle);
+	        //echo '<script>alert("'.is_user_logged_in().'0");</script>';
+	    }
+	    else 
+	    {   
+	    	//wp_generate_password(12,false);
+	    	wp_create_user($userhandle,wp_generate_password(12,false)  ,$useremail);
+	        
+	    	$qa_user = get_user_by('email', $useremail);
+	    	wp_set_auth_cookie($qa_user->ID, true, false);
+	        do_action('wp_login', $qa_user->user_login, $qa_user);
+	        
+	        echo '<script>window.location.reload(); </script>';
+	        exit;
+	    	//wp_set_current_user(null,$userhandle);
+	        //echo '<script>alert("'.$useremail.'1");</script>';
+	    }
+	
+	}
+	else 
+	{
+		wp_get_current_user();
+		if(qa_get_logged_in_email()!=$current_user->user_email)
+		{
+		echo '空间激活错误。';
+		exit;
+		};
+		//wp_redirect('http://'.$_SERVER['HTTP_HOST'].'?qa=login&to=wordpress');	
+	    //exit;
+	}
