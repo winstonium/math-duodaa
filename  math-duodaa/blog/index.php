fbe5e9aa58f2a3cd300aec03303fab4c00a33338
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-09-23 04:16:37 +0000

    飞机上写的blog上传。。。。。。。。。。。。

---  math-duodaa/blog/index.php
+++  math-duodaa/blog/index.php
@@ -11,6 +11,7 @@ require_once BLOG_ROOT.'/include/users.php';
 //语言与主题
 require_once BLOG_ROOT.'/lang/'.BLOG_LANG.'/lang.php';
 $blog_lang= new blog_lang();
+require_once BLOG_ROOT.'/theme/'.BLOG_THEME.'/theme.php';
 //require_once BLOG_ROOT;
 
 $blog_user_handle=trim(qa_get_logged_in_handle());
@@ -52,11 +53,14 @@ if($is_acted==0)
 
 $bqs=blog_get_query_str();
 
+$blog_page=new blog_page();
+$blog_page->pagetype=$bqs;
+$blog_page->print_page();
 
 //首页代码
 if($bqs=='home')
 {
-	echo is_activated($blog_user_handle);
+	//echo is_activated($blog_user_handle);
 	//echo is_activated('hahaha').'||';
 	
 	//$ats = blog_load_articles('math001','','id',0);
---  math-duodaa/blog/theme/default/theme.php
+++  math-duodaa/blog/theme/default/theme.php
@@ -4,5 +4,10 @@ if(!BLOG_VER)
 echo '<script>window.location.href="/";</script>';	
 exit;
 }
+
+$theme_dir = dirname(__FILE__);
+require_once dirname($theme_dir).'/default.php';
+
 ?>
+
 
