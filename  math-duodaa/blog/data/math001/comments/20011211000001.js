8d2d7c97d11808f4df9b948e1a67dae040b0889f
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-10-06 16:35:09 +0000

    ......................

---
+++  math-duodaa/blog/data/math001/comments/20011211000001.js
@@ -0,0 +1,27 @@
+﻿
+  {
+       "articlenum":"welcome",
+       "comments":
+       [
+         {
+		      "commentid":"2344555",
+        	  "author":"mamama",
+        	  "email":"setslei@163.com",
+		      "date":"2011-9-24 15:30",
+		      "content":"Welcome to XML guestbook我是中国人!!",
+		      "ownerreply":"haaaaaaaaaaaaaaaa"
+
+         }
+         ,
+         {
+          	  "commentid":"3344555",
+		      "author":"mamama",
+		      "email":"setslei@163.com",
+		      "date":"2011-9-24 15:30",
+		      "content":"Welcome to XML guestbook我是中国人!!",
+		      "ownerreply":"haaaaaaaaaaaaaaaa"
+
+        }
+       ]
+  
+  }
