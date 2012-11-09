4ffd849d3bf339151c2c6930f200697d273d08c3
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-11-09 01:40:56 +0000

    修改UI。。。。。。。。。。。。。

---
+++  math-duodaa/blog/theme/default/parts/header.php
@@ -0,0 +1,7 @@
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $page_title?></title>
+<link  rel="stylesheet" TYPE="text/css" href="<?php echo $css_src?>" />
+
+</head>
