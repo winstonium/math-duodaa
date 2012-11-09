fc982a7e31b208d5c6ebe0ad80c96ce710e1434d
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-11-09 01:43:22 +0000

    重新梳理入口页面的机制

--- trunk/ math-duodaa/blog/index.php
+++ trunk/ math-duodaa/blog/index.php
@@ -11,53 +11,18 @@ require_once BLOG_ROOT.'/include/strings.php';
 require_once BLOG_ROOT.'/include/articles.php';
 require_once BLOG_ROOT.'/include/users.php';
 
-//语言与主题
-//require_once BLOG_ROOT.'/lang/'.BLOG_LANG.'/lang.php';
-//$blog_lang= new blog_lang();
-//require_once BLOG_ROOT.'/include/urls.php';
-//$blog_urls= new blog_urls();
-require_once BLOG_ROOT.'/include/useritems.php';
-$uis=new useritems();
-require_once BLOG_ROOT.'/theme/'.BLOG_THEME.'/theme.php';
+//主题文件侠
+$themedir=BLOG_ROOT.'/theme/'.BLOG_THEME;
+require_once BLOG_ROOT.'/theme/default.php';
 
 
-//require_once BLOG_ROOT;
-
-$blog_user_handle=trim(qa_get_logged_in_handle());
-
-$is_acted=is_activated($blog_user_handle);
+$user=trim(qa_get_logged_in_handle());
 
 $bqs='';
 
 
-if($is_acted==-1)
-{
-	$bqs='blocked';
-    echo '<script>window.location.href="'.BLOG_NOTLOGIN_TRANSFER.'"</script>';
-	exit;
-
-}
-
-if($is_acted==0)
-{
-	$bqs='not_activated';
-    echo '<script>window.location.href="'.BLOG_NOTLOGIN_TRANSFER.'"</script>';
-	exit;
-
-}
-
-//require_once 'theme/'.BOLOG_THEME.'/theme.php';
-
-//echo $blog_user_handle;
-//echo BLOG_QAROOT;
-
-
-
 
 $bqs=blog_get_query_info();
-
-$blog_page->pagetype=$bqs;
-
 
 //首页代码
 
@@ -70,20 +35,8 @@ if($bqs['type']=='home')
 
 else if($bqs['type']=='user')
 {
-	//echo 'fffffff';
-	//echo $bqs['para1'];
-	if(!blog_is_userexist($bqs['para1']))
-	{
-		$er_msg = require BLOG_ROOT.'/lang/'.BLOG_LANG.'/lang.php';
-		echo $er_msg['ER_notexist'];
-		exit;
-	}
-	$blog_page->pagetype=$bqs['type'];
-	$blog_page->page_paras=$bqs;
-	$blog_page->print_page();
 	
-	//$list=blog_get_latescommentlist('math001');
-	//var_dump($list[0]);
+	require $themedir.'/pages/user.php';
 	exit;
 	
 }
