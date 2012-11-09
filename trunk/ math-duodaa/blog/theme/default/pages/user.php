b53018af258288a59e05c356f675ea2b8ce3c63d
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-11-09 01:40:56 +0000

    修改UI。。。。。。。。。。。。。

---
+++ trunk/ math-duodaa/blog/theme/default/pages/user.php
@@ -0,0 +1,29 @@
+<?php
+
+$user='';
+$page_title = '哆嗒数学网'.$user.'的空间';
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php require $themedir.'/parts/header.php';?>
+
+
+<body background="<?php echo $bg_img?>">
+<center>
+
+<?php 
+
+require $themedir.'/parts/blog_top.php';
+
+?>
+
+<div class="main_frame">
+
+</div>
+
+</center>
+</body>
+</html>
