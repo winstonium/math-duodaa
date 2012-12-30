e2fe25ad8ef80f4f6ccf145961de18a77d364d93
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2012-12-30 15:15:45 +0000

    删除。。。。。。。。。。。。。。。

---  math-duodaa/blog/theme/default/parts/foot.php
+++
@@ -1,3 +0,0 @@
-<div class="foot">
-<div class="copyright">哆嗒数学网 版权所有 © <?php echo date('Y')?> </div>
-</div>
---  math-duodaa/blog/theme/default/parts/header.php
+++
@@ -1,7 +0,0 @@
-
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?php echo $page_title?></title>
-<link  rel="stylesheet" TYPE="text/css" href="<?php echo $css_src?>" />
-
-</head>
