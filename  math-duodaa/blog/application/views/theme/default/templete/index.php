b75fd92d519088300c724ba0cfda95cc34420d6b
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2013-01-04 11:49:21 +0000

    修改。。。。。。。。。。。。。。。

---  math-duodaa/blog/application/views/theme/default/templete/index.php
+++  math-duodaa/blog/application/views/theme/default/templete/index.php
@@ -104,19 +104,22 @@
 #subbox ul
 {
 	padding-left:18px;
+	
 
 }
 #subbox ul li
 {
 	float:none;
 	text-align:left;
-	list-style-type:none;
+	list-style-type:disc;
 	padding-top:2px;
-	padding-bottom:3px;
-	padding-left:8px;
-	background-image: url(/blog/application/views/theme/default/img/index/small_items.gif);
-	background-position:-98px 12px;
-	background-repeat:no-repeat;
+	padding-left:3px;
+	margin-left:15px;
+	margin-bottom:4px;
+	color:#53301d;
+	font-size:8px;
+	
+	
 	
 	}
 #subbox ul li a
@@ -130,10 +133,44 @@
 	
 	}
 
+#recommended{ 
+background-color:#f8f5e8;
+padding-top:5px;
+padding-bottom:5px;
+}
+
+#recommended .title
+{
+	height: 30px;
+	width:100%;
+ 	background-image: url(/blog/application/views/theme/default/img/index/300width_items.gif);
+	background-repeat: no-repeat;
+	background-position:0 -10px;
+	text-align:left;
+	color:#834829;
+	padding-left:40px;
+	padding-top:5px;
+	}
+#recommended ul li{	
+	list-style:disc;
+	padding-left:3px;
+	vertical-align: middle;	
+	height:23px;
+	color:#53301d;
+	font-size:8px;
+	text-align:left;
+}
+#recommended ul li a
+{
+	color:#888;
+	font-size:15px;
+	
+	}
+
 
 #anounce
 {
-	background-color: #CCC;
+	background-color: #ddd;
 	padding-bottom:5px;
 	padding-top:10px;
 	margin: 10px;
@@ -144,17 +181,18 @@
 	padding-left:15px;
 	text-align:left;
 	list-style-type:none;
-	width:100%;}
+	width:100%;
+	margin-left:20px;
+	}
 	
 #anounce ul li
 {
-	list-style:none;
-	padding-left:7px;
-	background-image: url(/blog/application/views/theme/default/img/index/small_items.gif);
-	background-position:-98px 9px;
-	background-repeat:no-repeat;
-	height:23px;
-	
+	list-style: disc;
+	padding-left: 3px;
+	height: 23px;
+	color: #53301d;
+	font-size: 8px;
+	vertical-align: middle;	
 	}
 #anounce div.anounce_text
 {
@@ -202,17 +240,25 @@
                 
                  
                 </div> 
-                </div> 
+    </div> 
                  <!--轮播banner(完)-->
-				<div style="height:20px; width:100%;"></div>
+	<div style="height:20px; width:100%;"></div>
                 
-               <div>  </div>
+    <div id="recommended">  
+      <div class="title">推 荐 文 章</div>
+      <div class="links">
+      <ul>
+      <li><a href="#">math001：我们的祖国是花园</a></li>
+       <li><a href="#">math002：花园里花朵真鲜艳呀呀</a></li>
+      </ul>
+      </div>
+               </div>
 
 </div>
 <!-- 左侧条目(完) -->
 
 
-<div style="float:left; width:440px;height:500px; ">
+<div style="float:left; width:440px; ">
 <div id="ttBox">
 
 <div class="headline">
@@ -246,25 +292,36 @@
 
 
 </div>
-<div style="float:left; width:260px; height:500px;">
-<div id="anounce">
-<div><a href="#"><img style=" border: 1px solid #000;border-color:black;" src="/blog/application/views/theme/default/img/index/righttop2.gif" /></a>
-</div>
-<div class="anounce_text">
-<ul>
-<li><a href="#">哆嗒数学有奖征文</a></li>
-<li><a href="#">哆嗒数学征文</a></li>
-</ul>
-</div>
-</div>
+<div style="float:left; width:260px;">
+        <div id="anounce">
+        <div><a href="#"><img style=" border: 1px solid #000;border-color:black;" src="/blog/application/views/theme/default/img/index/righttop2.gif" /></a>
+        </div>
+        <div class="anounce_text">
+        <ul>
+        <li><a href="#">哆嗒数学有奖征文</a></li>
+        <li><a href="#">哆嗒数学征文</a></li>
+        </ul>
+        </div>
+        </div>
+       
+       <div style="width:250px; height:250px">
+       <script type="text/javascript">
+		/*250*250，创建于2013-1-4.blog首页，第一屏幕，左下方广告*/
+		var cpro_id = "u1180786";
+		</script>
+		<script src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></script>
+       </div> 
+       
+       <div class="clear" style="height:30px; width:200px" ></div>
 </div>
 
 
 
 
+
 </div>
 
-<div class="clear"></div>
+<div class="clear" style="width:1000px; height:30px"></div>
 
 
 
---
+++  math-duodaa/blog/index_config.js
@@ -0,0 +1,47 @@
+// JavaScript Document
+
+<index>
+{
+"ttboxes": [
+              {
+				"title":"aaaa",
+				"url":"bbb",
+				"relatives":[
+				              [
+							     {
+									  "title":"ccccccc",
+									  "url":"#"
+								 }
+							  ]
+							  ,
+							  [
+							     {
+									  "title":"ccccccc",
+									  "url":"#"
+								 }
+							  ]
+							]
+              },
+			  
+			  {
+				"title":"aaaa",
+				"url":"bbb",
+				"relatives":[
+				              [
+							     {
+									  "title":"哈哈哈哈啊啊啊啊啊啊啊啊啊啊啊啊啊",
+									  "url":"#"
+								 }
+							  ]
+							  ,
+							  [
+							     {
+									  "title":"ccccccc",
+									  "url":"#"
+								 }
+							  ]
+							]
+              }
+           ]
+
+}
