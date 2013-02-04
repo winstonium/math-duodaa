282ce091d3796317c6668c0d48c060f60c4e9269
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2013-02-04 10:14:58 +0000

    ,,,,,,,,,,,,,,,,,,,,,,,,,

--- trunk/ math-duodaa/blog/ckeditor/plugins/duodaa_math/dialogs/duodaa_math.js
+++ trunk/ math-duodaa/blog/ckeditor/plugins/duodaa_math/dialogs/duodaa_math.js
@@ -1,4 +1,5 @@
-﻿CKEDITOR.dialog.add( 'duodaa_math', function( editor ) {
+﻿var duodaa_math_edior_html='hahaha';
+CKEDITOR.dialog.add( 'duodaa_math', function( editor ) {
     return {
         title: "哆嗒数学公式编辑" ,
         minWidth: 500,
@@ -14,19 +15,19 @@
                     	//mathquill-embedded-latex 只做静态渲染
                         type: 'html',
                         //CKEDITOR.basePath
-                        //jmeditor1.0/ckeditor/plugins/jme/dialogs/mathdialog.html
-                        html: '<div style="width:500px;height:300px;"><iframe id="math_frame" style="width:500px;height:300px;" frameborder="no" src="' + CKEDITOR.basePath + 'plugins/jme/dialogs/mathdialog.html"></iframe></div>'
+						//html: duodaa_math_edior_html
+                        html: '<div style="width:500px;height:300px;"><iframe id="math_frame" style="width:500px;height:300px;" frameborder="no" src="' + CKEDITOR.basePath + 'plugins/duodaa_math/dialogs/mathdialog.html"></iframe></div>'
                     }   
                 ]
             }
         ],
         
 		onShow : function(){
-        	//$("#jme-math",getIFrameDOM("math_frame")).mathquill("focus");
+        	
         },
         onOk: function() {
         	var thedoc = document.frames ? document.frames('math_frame').document : getIFrameDOM("math_frame");
-        	var mathHTML = '<span class="mathquill-rendered-math" style="font-size:' + JMEditor.defaultFontSize + ';" >' + $("#jme-math",thedoc).html() + '</span><span>&nbsp;</span>';
+        	var mathHTML = '<span class="mathquill-rendered-math" style="font-size:' + '' + ';" >' +'xxxx' + '</span><span>&nbsp;</span>';
             editor.insertHtml(mathHTML);
 					return;
 			
