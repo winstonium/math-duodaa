78132de057e3fe8b910e5e8584af565663c081fc
setslei@163.com <setslei@163.com@661cd783-3d51-b229-52bc-861530c43137>
2013-08-05 14:28:17 +0000

    Json输出.......

--- trunk/ math-duodaa/qa-plugin/duodaa_data_for_client/data_question.php
+++ trunk/ math-duodaa/qa-plugin/duodaa_data_for_client/data_question.php
@@ -11,11 +11,30 @@ require_once QA_INCLUDE_DIR.'qa-db-selects.php';
 function duodaa_qlist($un=null,$type=null,$isanswered=false)
     {
 
-        $link=mysql_connect("localhost",QA_MYSQL_USERNAME,QA_MYSQL_PASSWORD);
-        mysql_select_db(QA_MYSQL_DATABASE);
 
-        $sql_query = 'SELECT IF(created > updated, created,updated) AS update_time FROM qa_posts ORDER BY update_time DESC';
+        $link=mysql_connect(QA_MYSQL_HOSTNAME,QA_MYSQL_USERNAME,QA_MYSQL_PASSWORD);
+        $slected = mysql_select_db(QA_MYSQL_DATABASE,$link);
 
-        $result=mysql_query($sql_query);
-        return $result;
+        $sql_query = 'SELECT *,IF(created < updated, updated,created) AS update_time '
+                    .'FROM qa_posts '
+                    .'WHERE type="Q" '
+                    .'ORDER BY update_time DESC LIMIT 0,30';
+
+        $result=mysql_query($sql_query,$link);
+        $qlist = mysql_fetch_array($result);
+        $i=0;
+        while($row = mysql_fetch_array($result))
+        {
+            $qlist[$i++] = $row;
+        }
+
+        mysql_close($link);
+
+        //转成Json
+        $qlist = json_encode($qlist);
+
+        //以下处理中文问题
+        $qlist = preg_replace("#\\\u([0-9a-f]{4})#ie", "iconv('UCS-2BE', 'UTF-8', pack('H4', '\\1'))", $qlist);
+
+        return $qlist;
      }
